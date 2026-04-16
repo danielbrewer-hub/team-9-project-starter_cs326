3ad8d4b85e3a9adae7534dad322ddea718155866
Aaron Tian <58406472+a2tian@users.noreply.github.com>
2026-04-16 02:20:07 -0400

    feat: add event service types

---
+++ src/events/errors.ts
@@ -0,0 +1,56 @@
+export type EventValidationError = {
+  name: "EventValidationError";
+  message: string;
+  field?: string;
+};
+
+export type EventAuthorizationError = {
+  name: "EventAuthorizationError";
+  message: string;
+};
+
+export type EventNotFoundError = {
+  name: "EventNotFoundError";
+  message: string;
+};
+
+export type EventUnexpectedDependencyError = {
+  name: "UnexpectedDependencyError";
+  message: string;
+};
+
+export type EventCreationError =
+  | EventValidationError
+  | EventAuthorizationError
+  | EventUnexpectedDependencyError;
+
+export type EventDetailError = EventNotFoundError | EventUnexpectedDependencyError;
+
+export function EventValidationError(message: string, field?: string): EventValidationError {
+  return {
+    name: "EventValidationError",
+    message,
+    field,
+  };
+}
+
+export function EventAuthorizationError(message: string): EventAuthorizationError {
+  return {
+    name: "EventAuthorizationError",
+    message,
+  };
+}
+
+export function EventNotFoundError(message: string): EventNotFoundError {
+  return {
+    name: "EventNotFoundError",
+    message,
+  };
+}
+
+export function UnexpectedDependencyError(message: string): EventUnexpectedDependencyError {
+  return {
+    name: "UnexpectedDependencyError",
+    message,
+  };
+}
